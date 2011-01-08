1778298d4f4e408158b5f920bcf1ec4e93efbc5a
Yingrui.F <yingrui.f@gmail.com>
2011-01-08 15:20:06 +0000

    1.3-SNAPSHOT

--- src/main/java/pinyin/PinyinToWord.java
+++ src/main/java/pinyin/PinyinToWord.java
@@ -2,10 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.apc.pinyin;
+package pinyin;
 
-import com.apc.pinyin.util.Pair;
-import com.apc.pinyin.util.Window;
+import pinyin.util.Pair;
+import pinyin.util.Window;
 import hmm.*;
 import hmm.ngram.TreeNode;
 import hmm.ngram.TreeNodeQuickSort;
