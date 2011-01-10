04d5708e4221bdd802fd2e761efc83b2d20fb398
Yingrui.F@gmail.com <Yingrui.F@gmail.com@3a5fec10-3e11-c50e-f634-1f28d4f54d9b>
2011-01-10 08:53:38 +0000

    some errors of corpus

--- trunk/src/main/java/hmm/Flag.java
+++ trunk/src/main/java/hmm/Flag.java
@@ -13,22 +13,24 @@ import java.util.Properties;
  */
 public class Flag {
 
-    private final Properties p = new Properties();
     private final static Flag flag = new Flag();
     public final int n = 3;
-    private final double[] labda = new double[n];
-    private final int ngramMinimumShowTimes = Integer.parseInt(p.getProperty("NgramMinimumShowTimes", "1"));
+    private final Properties p = new Properties();
+    private double[] labda;
+    private int ngramMinimumShowTimes;
 
     Flag() {
-        labda[0] = 1.0 / 6.0;
-        labda[1] = 1.0 / 3.0;
-        labda[2] = 1.0 / 2.0;
-
         try {
             p.load(Flag.class.getClassLoader().getResourceAsStream("pinyin.properties"));
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+
+        labda = new double[n];
+        labda[0] = 1.0 / 6.0;
+        labda[1] = 1.0 / 3.0;
+        labda[2] = 1.0 / 2.0;
+        ngramMinimumShowTimes = Integer.parseInt(p.getProperty("NgramMinimumShowTimes", "1"));
     }
 
     public static Flag getInstance() {
